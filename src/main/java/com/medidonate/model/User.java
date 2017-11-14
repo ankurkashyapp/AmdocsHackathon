554f19331a417f75b750de340b2b6ccc27e991af
Pravin Govardhan Barate <pravingo@amdocs.com>
2017-11-14 14:55:55 +0530

    sddsc

--- src/main/java/com/medidonate/model/User.java
+++ src/main/java/com/medidonate/model/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class User {
 
 	@Id
-	//@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.TABLE)
 	@Column(name="ID")
 	private Integer id;
 	
