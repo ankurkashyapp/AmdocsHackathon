d07ba04d40783b2bb59c5c010f3ea3d0912ef501
Pravin Govardhan Barate <pravingo@amdocs.com>
2017-11-14 16:15:55 +0530

    Chnged something

--- src/main/java/com/medidonate/model/User.java
+++ src/main/java/com/medidonate/model/User.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "USERS_DETAILS")
+@Table(name = "USERS_DETAILS", schema = "public")
 public class User {
 
 	@Id
