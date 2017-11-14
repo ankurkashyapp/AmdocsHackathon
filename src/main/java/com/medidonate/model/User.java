cc9e22bd068e442a181b5828f2b1e5cc52e32d89
Pravin Govardhan Barate <pravingo@amdocs.com>
2017-11-14 14:53:35 +0530

    Chnged something

--- src/main/java/com/medidonate/model/User.java
+++ src/main/java/com/medidonate/model/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	//@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="ID")
 	private Integer id;
 	
