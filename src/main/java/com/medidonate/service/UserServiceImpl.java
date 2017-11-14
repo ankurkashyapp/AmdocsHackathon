3de5f1c592e193b13919a9243395a93189213262
Pravin Govardhan Barate <pravingo@amdocs.com>
2017-11-14 14:32:08 +0530

    Added controller

--- src/main/java/com/medidonate/service/UserServiceImpl.java
+++ src/main/java/com/medidonate/service/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.medidonate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.medidonate.model.User;
 import com.medidonate.repository.UserRepository;
 import com.medidonate.utils.AbstractResponse;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
